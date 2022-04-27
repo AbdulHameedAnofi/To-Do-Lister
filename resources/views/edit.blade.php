5889660916c4fe1241df68f94b165bf9068420c3
Abdulhameed Anofi <anofiolawale@gmail.com>
2022-04-27 17:22:02 +0100

    styled

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -1,8 +1,16 @@
 
-<h1>Edit Activity</h1>
-<form action="/edit" method="POST">
-    @csrf
-    <input type="hidden" name="id" value="{{$event['id']}}">
-    <input type="text" name="event" value="{{$event['event']}}"><br><br>
-    <input type="submit" value="Edit">
+
+<link rel="stylesheet" href="{{url('css/app.css')}}">
+    <header>
+        <h1>To Do Lister</h1>
+    </header>
+    <div class="form">
+        <form action="/edit" method="POST">
+            <h2>Edit Activity</h2>
+            @csrf
+            <input type="hidden" name="id" value="{{$event['id']}}">
+            <input type="text" name="event" value="{{$event['event']}}" placeholder="enter activity"><br><br>
+            <button>Edit</button>
+        </form>
+    </div>
 </form>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,32 +4,29 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{url('css/app.css')}}">
     <title>Document</title>
 </head>
 <body>
-    <h1>To Do Lister</h1>
-    <form action="add" method="POST">
-        @csrf
-        <input type="text" name="event" placeholder="enter activity"><br><br>
-        <input type="submit" value="Add">
-    </form>
-
+    <header>
+        <h1>To Do Lister</h1>
+    </header>
+    <div class="form">
+        <h2>Activity</h2>
+        <form action="add" method="POST">
+            @csrf
+            <input type="text" name="event" placeholder="enter activity"><br><br>
+            <button>Add</button>
+        </form>
+    </div>
         @foreach($events as $event)
-            <p><a href={{"delete/".$event['id']}}>Delete</a>
-                      {{$event['event']}}             
-                      <a href={{"edit/".$event['id']}}>Edit</a></p>
+            <div class="activities">
+                <p><a href={{"delete/".$event['id']}}>Delete</a>
+                        {{$event['event']}}             
+                    <a href={{"edit/".$event['id']}}>Edit</a>
+                </p>
+            </div>
         @endforeach
 
 </body>
-<style>
-    h1 {
-        font: 300;
-        color: gray;
-    }
-
-    html {
-        /* background-color: #808080; */
-
-    }
-</style>
 </html>
